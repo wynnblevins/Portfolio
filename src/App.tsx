e0cf68b1144b5f55f6c655fdb6c12dfedcbae759
Robert Blevins <robertblevins@roberts-imac.mynetworksettings.com>
2024-06-04 12:30:38 -0400

    setting up page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Home, Navbar, Portfolio } from './components';
+import { Route, Routes } from 'react-router';
+import { NotFoundPage } from './components/NotFoundPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/home" element={<Home/>}></Route>
+        <Route path='/portfolio' element={<Portfolio />}></Route>
+        <Route path='*' element={<NotFoundPage />}></Route>
+      </Routes>
     </div>
   );
 }
