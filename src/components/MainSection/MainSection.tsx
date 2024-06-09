0895c7ad8e8e9441949203d2d80bbf6d652834d1
Robert Blevins <robertblevins@roberts-imac.mynetworksettings.com>
2024-06-09 12:54:40 -0400

    adding profile pic to about page

--- src/components/MainSection/MainSection.tsx
+++ src/components/MainSection/MainSection.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { withStyles, WithStyles } from "@material-ui/core";
+import profilePic from "../../images/profile-pic-150x150.png"
 
 interface MainSectionProps extends WithStyles {};
 
@@ -15,6 +16,10 @@ const styles = {
   },
   aboutHeader: {
     paddingTop: "20px",
+  },
+  profilePic: {
+    float: "left" as const,
+    margin: "20px"
   }
 };
 
@@ -24,13 +29,15 @@ const MainSectionBase = (props: MainSectionProps) => {
   return (
     <div className={classes.mainWrapper}>
       <h1 className={classes.aboutHeader}>About</h1>
-      <p className={classes.bioText}>Robert (Wynn) Blevins is a software developer from Lynchburg Virginia. After graduating from Lynchburg College, 
-        Wynn went to work for the Las Vegas based company TRAX International, then later moved to Alexandria Virginia to work for 
-        Terathink (formerly called Dominion Consulting). After relocating from Northern Virginia to attend the University of Richmond's 
-        Full Stack Developer Bootcamp, he found employment as a full stack developer in Richmond where he remained for five and a half years. 
-        In his spare time, Wynn enjoys playing his guitar and building various electronics projects. Wynn currently resides in Midlothian, Virginia 
-        with his wife Holly and their two cats Breeze and Wobbles.</p>
-
+      <div>
+        <img className={classes.profilePic} src={profilePic} alt="picture of Wynn Blevins"></img>
+        <p className={classes.bioText}>Robert (Wynn) Blevins is a software developer from Lynchburg Virginia. After graduating from Lynchburg College, 
+          Wynn went to work for the Las Vegas based company TRAX International, then later moved to Alexandria Virginia to work for 
+          Terathink (formerly called Dominion Consulting). After relocating from Northern Virginia to attend the University of Richmond's 
+          Full Stack Developer Bootcamp, he found employment as a full stack developer in Richmond where he remained for five and a half years. 
+          In his spare time, Wynn enjoys playing his guitar and building various electronics projects. Wynn currently resides in Midlothian, Virginia 
+          with his wife Holly and their two cats Breeze and Wobbles.</p>
+      </div>
       <p className={classes.bioText}>If you're interested in hiring Wynn, please feel free to download a copy of his resume using the link below.</p>
     </div>
   );
