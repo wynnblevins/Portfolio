c8106609cdaac3b07c85f9ac1215d396ececb545
Wynn Blevins <wynnblevins@gmail.com>
2024-12-06 14:38:19 -0500

    tweaking mobile navbar issues

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -57,7 +57,7 @@ const NavbarBase = (props: NavbarProps) => {
       <Container className={classes.container}>
         <Toolbar disableGutters >
           { userIsOnMobile() ? (
-            <Box sx={{ flexGrow: 1, display: { xs: 'flex', sm: 'flex' } }}>
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', sm: 'flex', md: 'flex', lg: 'flex', xl: 'flex' } }}>
               {/* This is the mobile toolbar interface */}
               <IconButton
                 aria-label="account of current user"
